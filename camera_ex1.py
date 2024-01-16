8ee004c6e9b165d5c763e2e3825b8716c97c1b4c
abbashassani1 <abbas1996@g.ucla.edu>
2024-01-16 10:18:29 -0800

    Part 6: camera excercises 1

--- camera_ex1.py
+++ camera_ex1.py
@@ -56,88 +56,92 @@
 
 # source of this code is a youtube video
 # https://www.youtube.com/watch?v=aFNDh5k3SjU
-# import cv2
-# import numpy as np
-# from PIL import Image
-
-# def get_limits(color):
-#     c = np.uint8([[color]])
-#     hsvC= cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-#     lowerLimit = hsvC[0][0][0] - 10, 100, 100
-#     upperLimit = hsvC[0][0][0] + 10, 255, 255
-
-#     lowerLimit = np.array(lowerLimit, dtype = np.uint8)
-#     upperLimit = np.array(upperLimit, dtype = np.uint8)
-#     return lowerLimit, upperLimit
-
-# yellow = [0, 100, 250]
-# cap = cv2.VideoCapture(0)
-
-
-# while True:
-#     ret, frame = cap.read()
-#     cv2.imshow('Frame', frame)
-
-#     hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-#     lowerLimit, upperLimit = get_limits(color = yellow)
-#     mask = cv2.inRange(hsv_img, lowerLimit, upperLimit)
-#     mask_ = Image.fromarray(mask)
-
-#     bound_box = mask_.getbbox()
-#     print(bound_box)
-#     if bound_box is not None:  # Change here
-#         x1, y1, x2, y2 = bound_box
-#         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
- 
-#     cv2.imshow('Mask', frame)
-
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-
 import cv2
 import numpy as np
 from PIL import Image
 
 def get_limits(color):
-    # Directly use the BGR color for limits in RGB space
-    # You need to define these limits based on the color you want to track
-    lowerLimit = [color[0] - 10, color[1] - 10, color[2] - 10] # Adjust these values
-    upperLimit = [color[0] + 10, color[1] + 10, color[2] + 10] # Adjust these values
+    c = np.uint8([[color]])
+    hsvC= cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
 
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
+    lowerLimit = np.array(lowerLimit, dtype = np.uint8)
+    upperLimit = np.array(upperLimit, dtype = np.uint8)
     return lowerLimit, upperLimit
+    k = (upperLimit - lowerLimit)
+    
+    print(k)
 
-# Define orange in BGR
-orange = [0, 110, 250]  # This is a typical representation of orange in BGR
+yellow = [0, 100, 250]
 cap = cv2.VideoCapture(0)
+
 
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
+    cv2.imshow('Frame', frame)
 
-    lowerLimit, upperLimit = get_limits(color=orange)
-    mask = cv2.inRange(frame, lowerLimit, upperLimit)
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color = yellow)
+    mask = cv2.inRange(hsv_img, lowerLimit, upperLimit)
     mask_ = Image.fromarray(mask)
 
     bound_box = mask_.getbbox()
-    print(bound_box)
-    if bound_box is not None:
+    #print(bound_box)
+    if bound_box is not None:  # Change here
         x1, y1, x2, y2 = bound_box
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-
+ 
     cv2.imshow('Mask', frame)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
 cap.release()
 cv2.destroyAllWindows()
+
+
+# #RGB text for video
+# import cv2
+# import numpy as np
+# from PIL import Image
+
+# def get_limits(color):
+#     # Directly use the BGR color for limits in RGB space
+#     # You need to define these limits based on the color you want to track
+#     lowerLimit = [color[0] - 10, color[1] - 10, color[2] - 10] 
+#     upperLimit = [color[0] + 10, color[1] + 10, color[2] + 10] 
+
+#     lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+#     upperLimit = np.array(upperLimit, dtype=np.uint8)
+#     return lowerLimit, upperLimit
+
+# # Define orange in BGR
+# orange = [0, 110, 250]  # This is a typical representation of orange in BGR
+# cap = cv2.VideoCapture(0)
+
+# while True:
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
+
+#     lowerLimit, upperLimit = get_limits(color=orange)
+#     mask = cv2.inRange(frame, lowerLimit, upperLimit)
+#     mask_ = Image.fromarray(mask)
+
+#     bound_box = mask_.getbbox()
+#     print(bound_box)
+#     if bound_box is not None:
+#         x1, y1, x2, y2 = bound_box
+#         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+#     cv2.imshow('Mask', frame)
+
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
